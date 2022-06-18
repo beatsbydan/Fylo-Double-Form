635299fab26d2702070038cbded8981b59cb2a84
beatsbydan <91322337+beatsbydan@users.noreply.github.com>
2022-06-18 22:03:52 +0100

    Add files via upload

--- validation.js
+++ validation.js
@@ -1,6 +1,6 @@
 function validation1(){
     var email = document.getElementById("mail"); 
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;   
+    var validRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;   
     if (email.value.match(validRegex)){
         email.style.borderColor = "green";
         alert("Form Submitted Successfully!")
@@ -14,7 +14,7 @@ function validation1(){
 }
 function validation2(){
     var emaill = document.getElementById("maill"); 
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;   
+    var validRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;   
     if (emaill.value.match(validRegex)){
         alert("Form Submitted Successfully!")
         return true;
